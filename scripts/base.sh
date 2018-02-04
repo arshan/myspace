a9e20e2890d42ce82b05f7d1aa1490815fccaf12
arshan <arshan@gmail.com>
2018-02-03 20:42:45 -0800

    it continues

---
+++ scripts/base.sh
@@ -0,0 +1,5 @@
+sudo apt-get install git
+git config --global user.email "arshan@gmail.com"
+git config --global user.name "arshan"
+
+sudo apt-get install emacs build-essential
---
+++ scripts/nvidia.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+
+sudo apt-get update
+
+echo now install the latest nvidia driver probably  >= 384 
