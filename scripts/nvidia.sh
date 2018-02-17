8d0351e869233de0bd408cfd946b7fb9c0cb8319
arshan <arshan@gmail.com>
2018-02-17 14:59:24 -0800

    fix typo

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -2,4 +2,4 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 
 sudo apt-get update
 
-echo now install the latest nvidia driver probably  >= 384 
+echo 'now install the latest nvidia driver probably  >= 384'
