d230116230bc04acf77bede5566e30c42f6ffa0a
arshan <arshan@gmail.com>
2018-09-05 16:39:33 -0700

    small updates

--- scripts/gui.sh
+++ scripts/gui.sh
@@ -3,3 +3,9 @@ sudo apt-get -y install gkrellm
 sudo apt-get -y install emacs vim terminator 
 sudo apt-get -y install xtightvncviewer
 
+# also install the official google-chrome
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+rm -f google-chrome*.deb
+
