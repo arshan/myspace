34d54c1dae27de5a1b2a0685c5bcdb03a33e9258
arshan <arshan@gmail.com>
2018-09-23 10:51:31 -0700

    add docker-compose install

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -13,6 +13,9 @@ sudo usermod -a -G docker $USER
 docker run hello-world
 docker pull ubuntu
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "IF DOCKER INSTALLED YOU SHOULD SEE A ROOT SHELL BELOW ... "
 echo " please 'exit' when your ready "
 docker run -it ubuntu bash
